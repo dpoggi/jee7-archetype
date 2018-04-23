1dd44fca757ea7c4dd0ea51d54354105c43b00b8
Dan Poggi <dan.poggi@gmail.com>
2018-04-23 19:37:37 -0400

    Verbose pom.xml, lean JaxRsApplication

--- src/main/resources/archetype-resources/src/main/java/JaxRsApplication.java
+++ src/main/resources/archetype-resources/src/main/java/JaxRsApplication.java
@@ -1,23 +1,8 @@
 package ${package};
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/")
 public class JaxRsApplication extends Application {
-
-    private final Map<String, Object> properties = new LinkedHashMap<>();
-
-    public JaxRsApplication() {
-        // To enable jersey-media-json-jackson in the presence of MOXy:
-        //properties.put("jersey.config.server.disableMoxyJson", true);
-    }
-
-    @Override
-    public Map<String, Object> getProperties() {
-        return properties;
-    }
 }
